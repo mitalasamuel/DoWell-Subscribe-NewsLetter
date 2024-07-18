02d76b5f78825a7b38d124791d8a2a1eec744de3
Mitala <mitalatechnologies@gmail.com>
2024-07-18 17:15:25 +0300

    Add

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:"export",
     basePath: '/DoWell-Subscribe-NewsLetter',
 reactStrictMode: true,
 };
--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,6 +1,5 @@
 import { SubscribersData } from "../../util";
 import { NextRequest, NextResponse } from "next/server";
-import { SubscribersData } from "../../util";
 import { Novu } from "@novu/node";
 
 //👇🏻 Novu SDK for sending emails
