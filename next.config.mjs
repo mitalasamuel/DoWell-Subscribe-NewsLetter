564d309e9b2f9d5a75a70b47df3bafed2fabcf6d
Mitala <mitalatechnologies@gmail.com>
2024-07-18 17:01:55 +0300

    Add

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/DoWell-Subscribe-NewsLetter',
+reactStrictMode: true,
+};
 
 export default nextConfig;
